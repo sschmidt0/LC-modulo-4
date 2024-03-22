b5a3424b995a89f9c45219bab366193fff3618d2
sakschmidt <sarah.schmidt@code.seat>
2024-03-22 14:27:15 +0100

    feat: add basic exercise - add input - add button - filter by organization

--- src/list.tsx
+++ src/list.tsx
@@ -9,28 +9,52 @@ interface MemberEntity {
 }
 
 export const ListPage: React.FC = () => {
+  const [organization, setOrganization] = React.useState("lemoncode");
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
 
-  // https://api.github.com/orgs/lemoncode/members
-  React.useEffect(() => {
-    fetch(`https://api.github.com/orgs/lemoncode/members`)
+  const fetchMembers = (organization: string) => {
+    fetch(`https://api.github.com/orgs/${organization}/members`)
       .then((response) => response.json())
       .then((json) => setMembers(json));
+  };
+
+  React.useEffect(() => {
+    fetchMembers(organization);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleChangeOrganization = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setOrganization(event.target.value);
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    fetchMembers(organization);
+  };
 
   return (
     <div className={styles.container}>
       <h2>Member list</h2>
+      <form className={styles["search-form"]}>
+        <input
+          name="search organization"
+          value={organization}
+          onChange={handleChangeOrganization}
+        />
+        <button onClick={handleClick}>Search for organization's members</button>
+      </form>
       <div className={styles["list-user-list-container"]}>
         <span className={styles["list-header"]}>Avatar</span>
         <span className={styles["list-header"]}>Id</span>
         <span className={styles["list-header"]}>Name</span>
         {members.map((member) => (
-          <>
+          <React.Fragment key={member.id}>
             <img src={member.avatar_url} />
             <span>{member.id}</span>
             <Link to={`/detail/${member.login}`}>{member.login}</Link>
-          </>
+          </React.Fragment>
         ))}
       </div>
       <Link to="/detail">Navigate to detail page</Link>
